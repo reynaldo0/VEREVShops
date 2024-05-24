2a59304de92e3794849a57292deff4b16763b64a
reynaldo0 <reynaldooyusellino@gmail.com>
2024-05-24 13:09:41 +0700

    text "images media"

--- verev/settings.py
+++ verev/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'theme/static/'
+STATIC_URL = '/static/'
 
 # untuk mengambil gambar dari db
 MEDIA_URL = '/media/'
