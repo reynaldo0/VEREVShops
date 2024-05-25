f6281ab47c6fcdf039b4e8e837c32bcb2faa834b
reynaldo0 <reynaldooyusellino@gmail.com>
2024-05-25 09:12:18 +0700

    text "fix all page and new file forms for auth user"

---
+++ verevapp/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm #lib auth dj
+
+class CustomerRegistForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'autofocus':'True',
+    'class':'form-control'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+    'class':'form-control'}))
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={
+    'class':'form-control'}))
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={
+    'class':'form-control'}))
--- verevapp/urls.py
+++ verevapp/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path("", views.home),
     path("home/", views.home, name='home'),
     path("about/", views.about, name='about'),
     path("contact/", views.contact, name='contact'),
